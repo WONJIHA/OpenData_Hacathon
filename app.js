6cdbaa44d4b47a25ec22b50c4bd68a9fa882a40c
COMDORI <02145s1@gmail.com>
2020-12-10 09:16:27 +0900

    2.5.1
    실시간 코로나 알림판 업데이트 및 파일 정리

--- app.js
+++ app.js
@@ -24,23 +24,3 @@ app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 // });
-
-// const http = require('http');
-// const fs = require('fs');
-
-// http.createServer((req, res) => {
-//     fs.readFile('./index.html', (err, data) => {
-//         if(err) {
-//             throw err;
-//         }
-//         res.end(data);
-//     });
-// }).listen(8080, () => {
-//     console.log('8080 포트 대기중');
-// });
-
-/*function hello() {
-    console.log('Hello World');
-}
-
-// hello();
